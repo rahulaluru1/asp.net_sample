ee1514a20697006a9a725aabc9a644ef0d161e95
Rahul Aluru <rahulaluru3@gmail.com>
2020-07-18 22:43:39 +0530

    first commit

---
+++ SampleWebApp/Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SampleWebApp.Models;
+namespace SampleWebApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie=new Movie(){ Name="Avengers" };
+            return View(movie);
+        }
+    }
+}
