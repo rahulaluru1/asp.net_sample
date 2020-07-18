d7481199bff762e8f04b312f5b7a5a3db7c37c4b
Rahul Aluru <rahulaluru3@gmail.com>
2020-07-19 00:00:41 +0530

    added new functionality and resolved dependabot alert

--- SampleWebApp/Controllers/MoviesController.cs
+++ SampleWebApp/Controllers/MoviesController.cs
@@ -4,15 +4,44 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SampleWebApp.Models;
+using SampleWebApp.ViewModel;
 namespace SampleWebApp.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie=new Movie(){ Name="Avengers" };
-            return View(movie);
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
     }
 }
